e00b80dd1c474923ce2df2f9561da0e234088000
Mateusz Krawiec <mateusz.krawiec6@gmail.com>
2017-11-02 18:09:01 +0100

    cat-rs: performance & bugfixes

--- cat/src/main.rs
+++ cat/src/main.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::io::{self, Write};
+use std::io::{self, Write, BufReader};
 use std::fs;
 use std::path;
 
@@ -12,15 +12,6 @@ macro_rules! cat_die {
         eprintln!(concat!("cat: ", $fmt));
         ::std::process::exit(1);
     });
-}
-
-/// Trait used to perform text decoration when special cat options are enabled,
-/// such as line numbering.
-trait Decorator {
-    /// Decorates the byte @ref byte writing the decoration into @p output. If the byte
-    /// is to be removed from the result the function returns true, otherwise it returns
-    /// false.
-    fn decorate(&mut self, byte: u8, output: &mut Vec<u8>) -> bool;
 }
 
 /// Classification of cat's arguments
@@ -76,49 +67,87 @@ static OPTIONS: &'static [CatArgDef] = &[
     },
 ];
 
+pub struct Decorators {
+    ends: bool,
+    number: bool,
+    squeeze: bool,
+}
+
+impl Decorators {
+    fn any(&self) -> bool {
+        return self.ends || self.number || self.squeeze;
+    }
+}
+
 fn copy_to_stdout_raw_or_die(from: &mut std::io::Read) {
-    io::copy(from, &mut io::stdout()).unwrap_or_else(|_| {
+    let stdout = io::stdout();
+    io::copy(from, &mut stdout.lock()).unwrap_or_else(|_| {
         cat_die!("I/O error");
     });
 }
 
-fn copy_to_stdout_decorated_or_die(from: &mut std::io::Read, decorators: &mut [Box<Decorator>]) {
+fn copy_to_stdout_decorated_or_die(reader: &mut std::io::Read, decorators: &Decorators) {
+    const BUFSIZE: usize = 8192;
+    let stdout = io::stdout();
+    let mut writer = stdout.lock();
+    let mut input: [u8; BUFSIZE] = [0u8; BUFSIZE];
+    let mut output: Vec<u8> = vec![];
+
+    let mut last_byte: i32 = -1;
+    let mut empty_streak: i32 = 1;
+    let mut current_line: i32 = 1;
+
     loop {
-        let mut input: Vec<u8> = Vec::with_capacity(8192);
-        let mut output: Vec<u8> = Vec::with_capacity(8192);
-
-        match from.read_to_end(&mut input) {
+        let len = match reader.read(&mut input) {
             Ok(0) => break,
-            Ok(_) => {
-                'character_loop: for character in input {
-                    for decorator in decorators.iter_mut() {
-                        if decorator.decorate(character, &mut output) {
-                            break 'character_loop;
-                        }
-                    }
+            Ok(len) => len,
+            Err(ref e) if e.kind() == io::ErrorKind::Interrupted => continue,
+            _ => cat_die!("I/O read error"),
+        };
 
-                    output.push(character);
+        for character in &input[..len] {
+            if decorators.squeeze {
+                if *character == '\n' as u8 {
+                    empty_streak += 1;
+
+                    if empty_streak >= 3 {
+                        continue;
+                    }
+                } else {
+                    empty_streak = 0;
                 }
-                io::stdout().write_all(&output).unwrap_or_else(
-                    |_| cat_die!("I/O error"),
-                );
             }
-            Err(_) => {
-                cat_die!("I/O error");
+            if decorators.number {
+                if last_byte < 0 || last_byte == '\n' as i32 {
+                    output.append(&mut format!("{:6}: ", current_line).into_bytes());
+                    current_line += 1
+                }
             }
+            if decorators.ends {
+                if *character == '\n' as u8 {
+                    output.push('$' as u8);
+                }
+            }
+            output.push(*character);
+            last_byte = *character as i32;
         }
+
+        writer.write_all(&output).unwrap_or_else(|_| {
+            cat_die!("I/O write error");
+        });
+        output.clear();
     }
 }
 
-fn copy_to_stdout_or_die(from: &mut std::io::Read, decorators: &mut [Box<Decorator>]) {
-    if decorators.len() > 0 {
+fn copy_to_stdout_or_die(from: &mut std::io::Read, decorators: &Decorators) {
+    if decorators.any() {
         copy_to_stdout_decorated_or_die(from, decorators)
     } else {
         copy_to_stdout_raw_or_die(from)
     }
 }
 
-fn get_file(name: &String) -> Box<fs::File> {
+fn get_file(name: &String) -> io::BufReader<fs::File> {
     match path::Path::new(name).metadata() {
         Err(e) => {
             match e.kind() {
@@ -137,13 +166,13 @@ fn get_file(name: &String) -> Box<fs::File> {
 
     return match fs::File::open(name) {
         Err(_) => cat_die!("{}: unknown error", name),
-        Ok(f) => Box::new(f),
+        Ok(f) => BufReader::new(f),
     };
 }
 
-fn cat_arg(arg: CatArg, decorators: &mut [Box<Decorator>]) {
+fn cat_arg(arg: CatArg, decorators: &Decorators) {
     let mut readable: Box<io::Read> = match arg {
-        CatArg::File(ref name) => get_file(name),
+        CatArg::File(ref name) => Box::new(get_file(name)),
         CatArg::Stdin => Box::new(io::stdin()),
         _ => return,
     };
@@ -155,7 +184,9 @@ fn show_help() {
         "Usage: {}: [OPTION]... [FILENAME]...",
         env::args().nth(0).unwrap()
     );
-    println!("Rust implementation of standard GNU cat. Concatenates FILE(s) to standard output.");
+    println!(
+        "Partial implementation of standard GNU cat. Concatenates FILE(s) to standard output."
+    );
     println!("");
     println!("With no FILE, or when FILE is -, read standard input.");
     for option in OPTIONS {
@@ -192,78 +223,6 @@ fn parse_args() -> Vec<CatArg> {
         .collect();
 }
 
-struct LineNumberDecorator {
-    current_line: i32,
-    last_byte: i32,
-}
-
-impl LineNumberDecorator {
-    fn new() -> LineNumberDecorator {
-        LineNumberDecorator {
-            current_line: 1,
-            last_byte: -1,
-        }
-    }
-}
-
-impl Decorator for LineNumberDecorator {
-    fn decorate(&mut self, byte: u8, output: &mut Vec<u8>) -> bool {
-        if self.last_byte < 0 || self.last_byte == '\n' as i32 {
-            output.append(&mut format!("{:6}: ", self.current_line).into_bytes());
-            self.current_line += 1;
-        }
-        self.last_byte = byte as i32;
-        return false;
-    }
-}
-
-struct EndLineDecorator;
-
-impl EndLineDecorator {
-    fn new() -> EndLineDecorator {
-        EndLineDecorator {}
-    }
-}
-
-impl Decorator for EndLineDecorator {
-    fn decorate(&mut self, byte: u8, output: &mut Vec<u8>) -> bool {
-        if byte == '\n' as u8 {
-            output.push('$' as u8);
-        }
-        return false;
-    }
-}
-
-struct LineSqueezerDecorator {
-    last_byte: i32,
-    streak: i32,
-}
-
-impl LineSqueezerDecorator {
-    fn new() -> LineSqueezerDecorator {
-        LineSqueezerDecorator {
-            last_byte: -1,
-            streak: 0,
-        }
-    }
-}
-
-impl Decorator for LineSqueezerDecorator {
-    fn decorate(&mut self, byte: u8, _: &mut Vec<u8>) -> bool {
-        if self.last_byte == '\n' as i32 && byte == '\n' as u8 {
-            self.streak += 1;
-
-            if self.streak != 1 {
-                return true;
-            }
-        } else {
-            self.streak = 0;
-        }
-        self.last_byte = byte as i32;
-        return false;
-    }
-}
-
 fn main() {
     let mut args = parse_args();
 
@@ -271,25 +230,27 @@ fn main() {
         return show_help();
     }
     if args.iter().any(|e| *e == CatArg::Version) {
-        return println!("cat, rust implementation - version 1.0");
+        return println!("Partial implementation of GNU cat, version 1.0.1");
     }
 
-    let mut decorators: Vec<Box<Decorator>> = vec![];
-    if args.iter().any(|e| *e == CatArg::Squeeze) {
-        decorators.push(Box::new(LineSqueezerDecorator::new()))
-    }
-    if args.iter().any(|e| *e == CatArg::Number) {
-        decorators.push(Box::new(LineNumberDecorator::new()));
-    }
-    if args.iter().any(|e| *e == CatArg::Ends) {
-        decorators.push(Box::new(EndLineDecorator::new()));
-    }
+    let decorators = Decorators {
+        ends: args.iter().any(|e| *e == CatArg::Ends),
+        number: args.iter().any(|e| *e == CatArg::Number),
+        squeeze: args.iter().any(|e| *e == CatArg::Squeeze),
+    };
 
-    if args.len() == 0 {
+    let mut has_any_input = false;
+    for arg in args.iter() {
+        match *arg {
+            CatArg::File(_) | CatArg::Stdin => has_any_input = true,
+            _ => {}
+        }
+    }
+    if !has_any_input {
         args.push(CatArg::Stdin);
     }
 
     for arg in args {
-        cat_arg(arg, &mut decorators);
+        cat_arg(arg, &decorators);
     }
 }
